95fe955abcf9eb7a9140b50759b9901518d7794e
Sergej Drus <90596978+tikserziku@users.noreply.github.com>
2024-11-27 14:27:14 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,9 +8,17 @@ document.getElementById('submitButton').addEventListener('click', () => {
         },
         body: JSON.stringify({ text: text })
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(errorInfo => Promise.reject(errorInfo));
+        }
+        return response.json();
+    })
     .then(data => {
         document.getElementById('outputText').innerText = data.processed_text;
     })
-    .catch(error => console.error('Ошибка:', error));
+    .catch(error => {
+        console.error('Ошибка:', error);
+        document.getElementById('outputText').innerText = 'Произошла ошибка при обработке запроса.';
+    });
 });
